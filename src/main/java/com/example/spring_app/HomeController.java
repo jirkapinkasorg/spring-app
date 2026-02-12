4a5bfdab1192f6d6589ed84bf3a13846dbbce839
jirkapinkas <jirka.pinkas@gmail.com>
2026-02-12 20:59:54 +0100

    send test to kafka

--- src/main/java/com/example/spring_app/HomeController.java
+++ src/main/java/com/example/spring_app/HomeController.java
@@ -1,17 +1,29 @@
 package com.example.spring_app;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @Value("${app.data}")
-    private String appData;
+    private final String appData;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public HomeController(@Value("${app.data}") String appData, KafkaTemplate<String, String> kafkaTemplate) {
+        this.appData = appData;
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     @GetMapping("/")
     public String home() {
         return appData;
+    }
+
+    @GetMapping("/test-kafka")
+    public String testKafka() {
+        kafkaTemplate.send("test_topic", "test", "test value");
+        return "Message sent to test_topic";
     }
 }
